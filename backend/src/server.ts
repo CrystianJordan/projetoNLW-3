bf93a2f47716bfc0c7827204f7122c669185f185
Crystian Jordan <crystian.jordan@catolicasc.org.br>
2020-10-13 23:42:00 -0300

    second commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { request, response } from 'express';
 import path from 'path';
 import 'express-async-errors';
+import cors from 'cors';
 
 import './database/connection';
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
+app.use(cors);
 
 
 app.listen(3333);
